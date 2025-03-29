033a0f3174e25bff073852dac558e5abc2083095
janker1747 <kin4ev.jank@gmail.com>
2025-03-29 14:13:43 +0300

    fpaskfpask

--- Assets/Script/general/ObjectPool.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IObjectPool<PrefabParrent> where PrefabParrent : MonoBehaviour
-{
-    PrefabParrent GetObject(Vector3 position, Quaternion rotation);
-    void ReturnObject(PrefabParrent obj);
-}
